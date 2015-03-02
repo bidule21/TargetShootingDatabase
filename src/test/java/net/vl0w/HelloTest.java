04f94997fdc5efb80f2bd0a7811b7b3e9a542753
Jonas Hansen <jonas.hansen@bluewin.ch>
2015-03-02 08:55:06 +0100

    Initial commit

---
+++ src/test/java/net/vl0w/HelloTest.java
@@ -0,0 +1,30 @@
+package test.java.net.vl0w;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.ws.rs.core.Application;
+
+import main.java.net.vl0w.Hello;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+public class HelloTest extends JerseyTest {
+
+	@Override
+	protected Application configure() {
+		return new ResourceConfig(Hello.class);
+	}
+
+	/**
+	 * Test to see that the message "Got it!" is sent in the response.
+	 */
+	@Test
+	public void testGetIt() {
+		final String responseMsg = target().path("myresource").request()
+				.get(String.class);
+
+		assertEquals("Hello, Heroku!", responseMsg);
+	}
+}
