6cb4a163035dc4c0296fe57d114fad441cad94bd
Jonas hansen <jonas.hansen@bluewin.ch>
2015-05-02 19:03:41 +0200

    Use double quotes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 /// <reference path="../typings/express.d.ts"/>
-var express = require('express');
+var express = require("express");
+var gateways = require("./gateways");
 var router = express.Router();
+router.use(gateways.GetGateway);
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Target Shooting Database', environment: process.env.SYSTEM_ENVIRONMENT });
+router.get("/", function (req, res, next) {
+    res.render("index", { title: "Target Shooting Database", environment: process.env.SYSTEM_ENVIRONMENT });
 });
 module.exports = router;
 //# sourceMappingURL=index.js.map
--- routes/index.ts
+++ routes/index.ts
@@ -1,15 +1,18 @@
 /// <reference path="../typings/express.d.ts"/>
 
-var express = require('express');
+import express = require("express");
+var gateways = require("./gateways");
 var router = express.Router();
 
+router.use(gateways.GetGateway);
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index',
-      { title: 'Target Shooting Database',
-        environment: process.env.SYSTEM_ENVIRONMENT
-      }
-  );
+router.get("/", function(req, res, next) {
+        res.render("index",
+            { title: "Target Shooting Database",
+                environment: process.env.SYSTEM_ENVIRONMENT
+            }
+        );
 });
 
 export = router;
