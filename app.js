8e504fcbe78a2bec6a25bc40b6423f3f25996561
Jonas hansen <jonas.hansen@bluewin.ch>
2015-05-04 13:37:38 +0200

    Deleted unnecessary routes

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 var gateways = require('./routes/gateways');
 
 var app = express();
@@ -27,7 +26,6 @@ app.use(gateways.handleGetRequest);
 app.use(gateways.handlePutRequest);
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
