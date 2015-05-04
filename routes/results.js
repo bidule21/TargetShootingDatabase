e40ca00c4113c46652773a30deed185b0e7e6361
Jonas hansen <jonas.hansen@bluewin.ch>
2015-05-04 14:11:06 +0200

    Add route "results"

--- routes/results.js
+++ routes/results.js
@@ -4,7 +4,6 @@
 /// <reference path="../typings/express.d.ts"/>
 var express = require("express");
 var router = express.Router();
-/* GET home page. */
 router.get("/", function (req, res, next) {
     res.render("results");
 });
--- routes/results.ts
+++ routes/results.ts
@@ -6,7 +6,6 @@
 import express = require("express");
 var router = express.Router();
 
-/* GET home page. */
 router.get("/", function(req, res, next) {
     res.render("results");
 });
