73c8ce1b359dda51e706e454ea57dba8e71988ed
Jonas Hansen <jonas.hansen@bluewin.ch>
2015-06-27 23:01:45 +0200

    Shooter path (just the skeleton)

--- api/controllers/shooter.js
+++ api/controllers/shooter.js
@@ -25,7 +25,7 @@ var util = require('util');
   we specify that in the exports of this module that 'hello' maps to the function named 'hello'
  */
 module.exports = {
-  hello: hello
+  getShooterById: getShooterById
 };
 
 /*
@@ -34,10 +34,10 @@ module.exports = {
   Param 1: a handle to the request object
   Param 2: a handle to the response object
  */
-function hello(req, res) {
+function getShooterById(req, res) {
   // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
-  var name = req.swagger.params.name.value || 'stranger';
-  var hello = util.format('Hello, %s!', name);
+  var id = req.swagger.params.id.value;
+  var hello = util.format('Querying for %s!', id);
 
   // this sends back a JSON response which is a single string
   res.json(hello);
