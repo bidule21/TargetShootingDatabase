8031c5c3b565d57b553410d89e72d690fcbeeb88
Jonas Hansen <jonas.hansen@bluewin.ch>
2015-06-27 22:51:38 +0200

    Created swagger project skeleton

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="../typings/express.d.ts"/>
-var express = require("express");
-var router = express.Router();
-/* GET home page. */
-router.get("/", function (req, res, next) {
-    res.render("index", { title: "Target Shooting Database", environment: process.env.NODE_ENV });
-});
-module.exports = router;
-//# sourceMappingURL=index.js.map
--- typings/test.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by jonashansen on 04/05/15.
- */
-
-/// <reference path="../typings/mocha.d.ts"/>
-/// <reference path="../typings/chai.d.ts"/>
-/// <reference path="../typings/sinon.d.ts"/>
