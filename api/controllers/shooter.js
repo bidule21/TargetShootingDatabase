b8d0787d130e3bf13139987e6baf30cc38ea7700
Jonas Hansen <jonas.hansen@bluewin.ch>
2015-07-20 21:13:55 +0200

    Added post functionality for shooters

--- api/controllers/shooter.js
+++ api/controllers/shooter.js
@@ -1,45 +1,37 @@
 'use strict';
-/*
- 'use strict' is not required but helpful for turning syntactical errors into true errors in the program flow
- http://www.w3schools.com/js/js_strict.asp
-*/
 
-/*
- Modules make it possible to import JavaScript files into your application.  Modules are imported
- using 'require' statements that give you a reference to the module.
-
-  It is a good idea to list the modules that your application depends on in the package.json in the project root
- */
-var util = require('util');
 var Shooter = require('../helpers/models.js').Shooter
 
-/*
- Once you 'require' a module you can reference the things that it exports.  These are defined in module.exports.
-
- For a controller in a127 (which this is) you should export the functions referenced in your Swagger document by name.
-
- Either:
-  - The HTTP Verb of the corresponding operation (get, put, post, delete, etc)
-  - Or the operationId associated with the operation in your Swagger document
-
-  In the starter/skeleton project the 'get' operation on the '/hello' path has an operationId named 'hello'.  Here,
-  we specify that in the exports of this module that 'hello' maps to the function named 'hello'
- */
 module.exports = {
-  getShooterById: getShooterById,
-  getAllShooters: getAllShooters
+    getShooterById: getShooterById,
+    getAllShooters: getAllShooters,
+    postShooterByName: postShooterByName
 };
 
 
 function getShooterById(req, res) {
-  var id = req.swagger.params.id.value;
-  Shooter.find({_id:id},function(err,shooter){
-    res.json(shooter);
-  });
+    var id = req.swagger.params.id.value;
+    Shooter.find({_id: id}, function (err, shooter) {
+        res.json(shooter);
+    });
 }
 
 function getAllShooters(req, res) {
-  Shooter.find(function(err,shooters){
-    res.json(shooters);
-  });
+    Shooter.find(function (err, shooters) {
+        res.json(shooters);
+    });
+}
+
+function postShooterByName(req, res) {
+    var shooter = new Shooter({
+        firstname: req.swagger.params.firstname.value,
+        lastname: req.swagger.params.lastname.value
+    });
+
+    shooter.save(function (err) {
+        if (err) {
+            res.status(500).json('Error while writing record. ' + err);
+        }
+        res.json(shooter);
+    });
 }
